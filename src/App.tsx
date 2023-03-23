9228c0873fe9bec57dbc691efe8bf52587984794
vlad <vnelipovich63@gmail.com>
2023-03-23 23:28:26 +0300

    rewrite counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,15 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/Counter/Counter";
-import Settings from "./components/Settings/Settings";
 
 
 function App() {
 
-    const [count, setCount] = useState<number>(0);
-
-    const incFunction = () => {
-
-    }
-    const resetFunc = () => {
-
-    }
-    const changeRed = {
-
-    }
-
-
-    const IncCountCallback = () => {
-        setCount(count + 1);
-    }
-    const ResetCallback = () => {
-        setCount(0)
-    }
-    const disabledCallback = () => count > 4
-
-    const ChangeRedCallback = () => {
-        color: count < 5 ? "black" : "red"
-    }
-
-  return (
-    <div className="App">
-        <Settings/>
-        <Counter count={count} IncCountCallback={IncCountCallback} ResetCallback={ResetCallback} />
-    </div>
-  );
-
-
+    return (
+        <div className="App">
+            <Counter/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,73 @@
-import React, {FC, useState} from 'react';
-import './Counter.css';
-import {Button} from "../Button"
-import Number from "./Number";
+import React, {ChangeEvent, useState} from "react";
+import './Counter.css'
+import '../Settings/Settings.css'
 
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [minValue, setMinValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(5);
 
-type AppPropsType = {
-    ResetCallback: () => void
-    IncCountCallback: () => void
-    count: number
-}
+    function increment() {
+        if (count < maxValue) {
+            setCount(count + 1);
+        }
+    }
 
-const Counter: FC<AppPropsType> = ({ResetCallback, IncCountCallback,count}) => {
+    function reset() {
+        setCount(minValue);
+    }
+
+    function setMinMaxValues() {
+        setCount(minValue);
+    }
+
+    function handleMinValueChange(event: ChangeEvent<HTMLInputElement>) {
+        setMinValue(Number(event.target.value));
+    }
+
+    function handleMaxValueChange(event: ChangeEvent<HTMLInputElement>) {
+        setMaxValue(Number(event.target.value));
+    }
+
+    function ChangeRed() {
+        return {color: count < maxValue ? 'black' : 'red'}
+    }
+
+    function disableButton() {
+        return count === maxValue
+    }
 
     return (
-        <div>
-            <Number count={count}/>
-            <Button IncCountCallback={IncCountCallback} ResetCallback={ResetCallback}></Button>
+        <div className='main'>
+            <div className='settingsMain'>
+                <div className='settingsWrapper'>
+                    <div className='startValue'>
+                        <h3>start value:</h3><input className='startInput' type="number"
+                                                    value={minValue}
+                                                    onChange={handleMinValueChange}/>
+                    </div>
+                    <div className='maxValue'>
+                        <h3>max value:</h3><input className='maxInput' type="number"
+                                                  value={maxValue}
+                                                  onChange={handleMaxValueChange}/>
+                    </div>
+                </div>
+                <div className='btnWrap'>
+                    <button onClick={setMinMaxValues} className='setButton'>Set</button>
+                </div>
+            </div>
+
+            <div className='CounterMain'>
+                <div className='NumWrap'>
+                    <h1 style={ChangeRed()}>{count}</h1>
+                </div>
+                <div className='CounterBtnsWrap'>
+                    <button onClick={increment} className='inc' disabled={disableButton()}>Increment</button>
+                    <button onClick={reset} className='reset'>Reset</button>
+                </div>
+            </div>
         </div>
     );
-};
+}
 
 export default Counter;
