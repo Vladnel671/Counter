68440785b208cbe9c920576ad137db46d6392bb2
vlad <vnelipovich63@gmail.com>
2023-04-01 02:35:49 +0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/Counter/Counter";
 import Counter2 from "./components/Counter2/Counter2";
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
 
 
 function App() {
-
     return (
         <div className="App">
-            {/*<Counter/>*/}
-            <Counter2/>
+            <div className='header'>
+                <NavLink to="/counter"  style={{textDecoration: "none"}}><h3 className='counter'>Counter</h3></NavLink>
+                <NavLink to="/counterV2" style={{textDecoration: "none"}}><h3 className='counterV2'>Counter V2</h3></NavLink>
+            </div>
+            <Routes>
+                <Route path='/' element={<Counter/>}/>
+                <Route path='/counterV2' element={<Counter2/>}/>
+                <Route path='/counter' element={<Counter/>}/>
+            </Routes>
         </div>
     );
 }
